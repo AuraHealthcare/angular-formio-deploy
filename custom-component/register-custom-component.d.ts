724d70427d5813ab08f021379fb69cda906e7afe
Balint Mero <balint.mero@codingsans.com>
2019-09-24 12:58:10 +0200

    Build

--- custom-component/register-custom-component.d.ts
+++ custom-component/register-custom-component.d.ts
@@ -1,4 +1,5 @@
 import { Injector, Type } from '@angular/core';
 import { FormioCustomComponentInfo } from '../formio.common';
+export declare function registerCustomTag(tag: string, injector: Injector): void;
 export declare function registerCustomFormioComponent(options: FormioCustomComponentInfo, angularComponent: Type<any>, injector: Injector): void;
 export declare function registerCustomFormioComponentWithClass(options: FormioCustomComponentInfo, angularComponent: Type<any>, formioClass: any, injector: Injector): void;
--- custom-component/register-custom-component.js
+++ custom-component/register-custom-component.js
@@ -7,13 +7,21 @@ import { Components } from 'formiojs';
 import { createCustomFormioComponent } from './create-custom-component';
 import { CustomTagsService } from './custom-tags.service';
 /**
+ * @param {?} tag
+ * @param {?} injector
+ * @return {?}
+ */
+export function registerCustomTag(tag, injector) {
+    injector.get(CustomTagsService).addCustomTag(tag);
+}
+/**
  * @param {?} options
  * @param {?} angularComponent
  * @param {?} injector
  * @return {?}
  */
 export function registerCustomFormioComponent(options, angularComponent, injector) {
-    injector.get(CustomTagsService).addCustomTag(options.selector);
+    registerCustomTag(options.selector, injector);
     /** @type {?} */
     var complexCustomComponent = createCustomElement(angularComponent, { injector: injector });
     customElements.define(options.selector, complexCustomComponent);
@@ -27,7 +35,7 @@ export function registerCustomFormioComponent(options, angularComponent, injecto
  * @return {?}
  */
 export function registerCustomFormioComponentWithClass(options, angularComponent, formioClass, injector) {
-    injector.get(CustomTagsService).addCustomTag(options.selector);
+    registerCustomTag(options.selector, injector);
     /** @type {?} */
     var complexCustomComponent = createCustomElement(angularComponent, { injector: injector });
     customElements.define(options.selector, complexCustomComponent);
